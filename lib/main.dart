3224695d7960bf6c7669c38ad2e3ceafe3540663
pu <nampu@finema.co>
2023-12-07 13:21:45 +0700

    add tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -29,15 +29,76 @@ class HomePage extends StatefulHookConsumerWidget {
   ConsumerState<ConsumerStatefulWidget> createState() => _HomePageState();
 }
 
-class _HomePageState extends ConsumerState<HomePage> {
+class _HomePageState extends ConsumerState<HomePage> with TickerProviderStateMixin {
+  late final TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final AsyncValue<List<CardWithStatement>> cardList = ref.watch(fetchCardListProvider);
     final AsyncValue<List<Statement>> billedList = ref.watch(billedStatementListProvider('4568111122223333'));
     return Scaffold(
       body: Center(
-        child: switch (billedList) {
-          AsyncData(:final value) => Text('cards: ${value.length}'),
+        child: switch (cardList) {
+          AsyncData(:final value) => Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Expanded(
+                  child: PageView.builder(
+                    itemCount: value.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Image.network(
+                        value[index].card.cardImageUrl,
+                      );
+                    },
+                    onPageChanged: (int value) {
+                      print(value.toString());
+                    },
+                  ),
+                ),
+                TabBar(
+                  controller: tabController,
+                  tabs: const <Widget>[
+                    Tab(
+                      child: Text('ACCOUNT INFO'),
+                    ),
+                    Tab(
+                      child: Text('UNBILLED'),
+                    ),
+                    Tab(
+                      child: Text('BILLED'),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: TabBarView(
+                    controller: tabController,
+                    children: const <Widget>[
+                      Center(
+                        child: Text("It's cloudy here"),
+                      ),
+                      Center(
+                        child: Text("It's rainy here"),
+                      ),
+                      Center(
+                        child: Text("It's sunny here"),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           AsyncError() => const Text('Oops, something unexpected happened'),
           _ => const CircularProgressIndicator(),
         },
